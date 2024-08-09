18f622092277c55d738b783fda92fe11b929820f
John Benninghoff <jbenninghoff@mac.com>
2024-08-09 11:05:13 -0500

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 # use rbenv to install base ruby stack (ruby, gem, bundler)
 set -ex # halt script on error, echo on
 
-rbenv install `cat .ruby-version`
+# NOTE: rbenv not needed for current versions used with `brew rbenv-sync`
+# rbenv install "$(cat .ruby-version)"
 bundle install
 bundle package
