1b969effbf9d85f73a01a15f94d805384834b63c
John Benninghoff <jbenninghoff@mac.com>
2024-08-09 13:45:20 -0500

    Fix GitHub Pages build warning

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@ set -ex # halt script on error, echo on
 
 # NOTE: rbenv not needed for current versions used with `brew rbenv-sync`
 # rbenv install "$(cat .ruby-version)"
+bundle config set --local with ci
 bundle install
 bundle package
