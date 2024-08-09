e045990f8d64af7d2e784b54db7701d25519848b
John Benninghoff <jbenninghoff@mac.com>
2024-08-09 15:01:09 -0500

    Use per-project gems

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,9 @@ set -ex # halt script on error, echo on
 
 # NOTE: rbenv not needed for current versions used with `brew rbenv-sync`
 # rbenv install "$(cat .ruby-version)"
-bundle config set --local with ci
+
+# per-project gems, see https://guilhermesimoes.github.io/blog/installing-gems-per-project-directory
+bundle config --local path vendor/bundle
+bundle config --local with ci
 bundle install
 bundle package
